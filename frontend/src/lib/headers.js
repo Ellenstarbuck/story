e709226614a723a8f371398ab161302eb081391b
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-02-29 09:53:51 +0000

    added react

---
+++ frontend/src/lib/headers.js
@@ -0,0 +1,10 @@
+import Cookies from 'js-cookie'
+const csrftoken = Cookies.get('csrftoken')
+
+export const headers = {
+  common: {
+    'X-CSRF-TOKEN': csrftoken
+  },
+  // <!-- later on you can include this (remember to import it): -->
+  // <!-- headers: { Authorization: `Bearer ${getToken()}` } -->
+}
