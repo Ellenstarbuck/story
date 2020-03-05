25eea507f4e2acce0ecec088250dfd6eb14ea1da
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-03-05 14:04:48 +0000

    added

---
+++ frontend/build/precache-manifest.05164e6e9426a5bca00177359066f56e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759f6cb75af68dff55d5767344f56bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fb365269198f281f5d",
+    "url": "/static/css/2.5dabca3a.chunk.css"
+  },
+  {
+    "revision": "d7444ec1785dbc885fa8",
+    "url": "/static/css/main.f2b83af2.chunk.css"
+  },
+  {
+    "revision": "89fb365269198f281f5d",
+    "url": "/static/js/2.7d761559.chunk.js"
+  },
+  {
+    "revision": "03f571cd0c3e4eb469324779825a0883",
+    "url": "/static/js/2.7d761559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7444ec1785dbc885fa8",
+    "url": "/static/js/main.3291ec55.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "492ff4052f024567301d887cf9908bca",
+    "url": "/static/media/aboutUs.492ff405.png"
+  },
+  {
+    "revision": "4c4803e861f2dbf6980bc370e4c7e4d7",
+    "url": "/static/media/actualreg.4c4803e8.png"
+  },
+  {
+    "revision": "05f893b17f94e648e79eb29292a5c8e3",
+    "url": "/static/media/background.05f893b1.png"
+  },
+  {
+    "revision": "5fe5d9aa22fa9db64edb5c38627c487d",
+    "url": "/static/media/background.5fe5d9aa.jpg"
+  },
+  {
+    "revision": "0d85da356d65561525c1992218c83406",
+    "url": "/static/media/errorpage.0d85da35.png"
+  },
+  {
+    "revision": "91b6abde0c81f41f5fce0cacfc3f3e86",
+    "url": "/static/media/newstory.91b6abde.png"
+  },
+  {
+    "revision": "9003f55d6bbdf80a363effc207bf3bee",
+    "url": "/static/media/profilebg.9003f55d.png"
+  },
+  {
+    "revision": "ca7efcae6a68ee22f7fc6c6abba56b9d",
+    "url": "/static/media/register.ca7efcae.png"
+  }
+]);
