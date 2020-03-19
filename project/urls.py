fa2b3deafd6b1533bb49ff2a0defc8ec590dee84
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-03-19 16:49:26 +0000

    urlstuff

--- project/urls.py
+++ project/urls.py
@@ -18,11 +18,11 @@ from django.urls import path, include, re_path
 from .views import index
 
 urlpatterns = [
-    # path('', index),
-    re_path(r'^(?P<path>.*)/$', IndexView.as_view()),
+    path('', index),
     path('django-admin/', admin.site.urls),
     path('admin/', include('rest_framework.urls')),
     path('api/storys/', include('storys.urls')),
     path('api/genres/', include('genres.urls')),
-    path('api/', include('jwt_auth.urls'))
+    path('api/', include('jwt_auth.urls')),
+    re_path(r'^.*$', index)
     ]
